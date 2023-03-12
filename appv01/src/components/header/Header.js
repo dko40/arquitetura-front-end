13c1725572eaa37c1739d6c4a3235e024225eb3e
adelson <deko.xp.38@gmail.com>
2023-03-12 16:15:58 -0300

    alterações e criação do header

---
+++ appv01/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Header = () => (
+
+    <div class="header">
+            <img src="./logo.png" alt="" class="logo"></img>
+            <ul class="menu">
+                <li class="item-menu">Link 1</li>
+                <li class="item-menu">Link 2</li>
+                <li class="item-menu">Link 3</li>
+                <li class="item-menu">Link 4</li>
+                <li class="item-menu">Link 5</li>
+            </ul>            
+        </div>
+)
+
+export default Header;
